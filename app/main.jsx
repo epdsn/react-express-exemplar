82d7c8c8b2bcd4a3f4b99c7936c617db146a3ddf
Eric Perez <ibestudios@gmail.com>
2018-06-10 08:50:27 -0700

    First inital push. created grocery app.

---
+++ app/main.jsx
@@ -0,0 +1,20 @@
+var React = require('react/addons');
+
+console.log('hello from jsx');
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+var GroceryItemStore = require('./stores/GroceryItemStore.jsx');
+
+var initial = GroceryItemStore.getItems();
+
+function render(){
+    React.render(<GroceryItemList items={initial} />, app)
+}
+
+GroceryItemStore.onChange(function(items){
+    initial = items;
+    render();
+})
+
+render();
