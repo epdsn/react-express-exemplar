57ba97d033292d0bd79e7684aed30299555bbbc9
Eric Perez <ibestudios@gmail.com>
2018-06-11 21:45:43 -0700

    added mongoos / mongdb backend

--- app/helpers/RestHelper.js
+++ app/helpers/RestHelper.js
@@ -22,5 +22,26 @@ module.exports = {
                 error:error
             })
         })
+    },
+    patch: function(url,data){
+        return new Promise(function(success, error){
+            $.ajax({
+                url: url,
+                type: "PATCH",
+                data:data,
+                success: success,
+                error:error
+            })
+        })
+    },
+    del: function(url){
+        return new Promise(function(success, error){
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: success,
+                error:error
+            })
+        })
     }
 }
